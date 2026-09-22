fb42fad196cb710bdef0b5d722fa604daba364ca
obinalivictor <obinalivictor3@gmail.com>
2026-09-22 13:29:16 +0100

    queue data structure

--- src/queue.java
+++ src/queue.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class queue {
-}
+    public class queue {
+    private ArrayList<Integer>queueContent = new ArrayList<>();
+
+
+    public int getSize(){
+        return queueContent.size();
+    }
+
+
+
+
+        public boolean isEmpty() {
+
+            return queueContent.isEmpty();
+        }
+
+        public void enqueue(int number) {
+        queueContent.add(number);
+        }
+
+        public int dequeue() {
+        if(isEmpty()){
+            throw new IllegalStateException("queue is empty");
+        }
+            return queueContent.remove(0);
+        }
+
+        public int peek() {
+        if(isEmpty()){
+            throw new IllegalStateException("queue is empty");
+        }
+            return queueContent.get(0);
+        }
+    }
+
+
--- testSource/quequeTest.java
+++ testSource/quequeTest.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class quequeTest {
-}
+
+    @Test
+    public void TesThatDefaultValueOfSizeIsZero(){
+
+        queue myqueue = new queue();
+        assertEquals(0,myqueue.getSize());
+    }
+
+    @Test
+    public void TestForSizeOfTheArray(){
+        queue myqueue = new queue();
+        assertTrue(myqueue.isEmpty());
+    }
+        @Test
+        public void TestThatAddingAnItemCanBeAddedSuccessfully(){
+            queue myqueue = new queue();
+            assertTrue(myqueue.isEmpty());
+            assertEquals(0,myqueue.getSize());
+
+            myqueue.enqueue(23);
+            assertFalse(myqueue.isEmpty());
+            assertEquals(1,myqueue.getSize());
+        }
+
+        @Test
+        public void TestRemovalOfItemFromAList(){
+            queue myqueue = new queue();
+            myqueue.enqueue(23);
+            myqueue.enqueue(4);
+            myqueue.enqueue(53);
+            myqueue.enqueue(73);
+            assertEquals(4,myqueue.getSize());
+
+            myqueue.dequeue();
+
+            assertEquals(3,myqueue.getSize());
+        }
+
+        @Test
+        public void TestThatYouCanPeekAtAnFrontOfQueue(){
+            queue myqueue = new queue();
+
+            myqueue.enqueue(23);
+            myqueue.enqueue(4);
+            myqueue.enqueue(53);
+            myqueue.enqueue(73);
+
+            assertEquals(23,myqueue.peek());
+
+        }
+    }
+
